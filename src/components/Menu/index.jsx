19dcc18ac25dbd882e604ef6aca2469fd6d93caa
Jana Šimíčková <jana.oravska@gmail.com>
2025-06-24 19:38:58 +0200

    Zprovoznění API a nahrání všech káv na web

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -1,7 +1,7 @@
 import "./index.css"
 import { Drink } from "../Drink"
 
-export const Menu = () => {
+export const Menu = ({ drinks }) => {
   return (
     <section className="menu" id="menu">
       <div className="container">
@@ -10,15 +10,17 @@ export const Menu = () => {
           Vyberte si z našeho interaktivního menu a nemusíte čekat na obsluhu
         </p>
         <div className="drinks-list">
-          <Drink 
-            id={0} 
-            name="Espresso" 
-            ordered={false} 
-            image="../pages/img/beans.png" 
-            layers={[ {
-              color: '#613916',
-              label: 'espresso',
-            } ]} />
+          {drinks.map((drink) => (
+            <Drink 
+            key={drink.id}
+            id={drink.id} 
+            name={drink.name} 
+            ordered={drink.ordered} 
+            image={`http://localhost:4000${drink.image}`} 
+            layers={drink.layers}
+            />
+          ))}
+          
         </div>
 
         <div className="order-detail">
